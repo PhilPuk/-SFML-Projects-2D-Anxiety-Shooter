126f89a22fa64fd762157d35f3e3511811e76d27
Phil_Puk <107513863+PhilPuk@users.noreply.github.com>
2022-08-23 10:21:14 +0200

    Used inside coming shop and game
    Shop: For buying upgrades
    Game: Add income while playing and killing enemies

---
+++ [SFML Projects] Mouse Shooter/Money.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include<iostream>
+#include<sstream>
+#include<SFML/Graphics.hpp>
+#include<SFML/System.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/Audio.hpp>
+#include<SFML/Network.hpp>
+
+class Money
+{
+private:
+
+  //Used for saving all income
+  float Bank;
+  //Used for saving income while playing
+  //Adds to bank once game is over
+  float IncomePerGame;
+  
+public:
+  Money();
+  virtual ~Money();
+  
+  //Accessors
+  const float& getBank() const;
+  const float& getIncomePerGame() const;
+  
+  //Modifiers
+  void IncreaseIncomePerGame(float Income);
+};
