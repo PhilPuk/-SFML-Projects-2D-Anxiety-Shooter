74f58f3057c3774dda473f9b677152f637a4ee90
Phil_Puk <107513863+PhilPuk@users.noreply.github.com>
2022-09-13 13:50:50 +0200

    Update bloom.h

--- [SFML Projects] Mouse Shooter/bloom.h
+++ [SFML Projects] Mouse Shooter/bloom.h
@@ -15,6 +15,10 @@ private:
   sf::CircleShape baseBloom;
   std::vector<sf::CircleShape*> blooms;
   
+  //Animations
+    //Scale animation
+    bool scalebigger;
+    float ScaleAnimationAmount;
   
   void initVariables();
   void initBaseBloom();
@@ -27,9 +31,22 @@ public:
   //Modifiers
   
   //Functions
-  void createNewBloom(sf::Vector2f& BloomPos, sf::Color color, float opacity);
+  void createNewBloom(sf::Vector2f& BloomPos, sf::Color color, float radius);
+  void deleteSpecificBloom(int index);
   
-  void update();
+  void updateBloomScaleAnimation(int i);
+  void updateBloomMovement(sf::Vector2f& movement[], int i);
+  void updateForLoop(sf::Vector2f& movement[]);
+  
+  /*Movement Array Order
+  0. Player Bloom
+  1. HP Bar bloom
+  2. Bullets
+  ...
+  3. Enemies
+  ...
+  */
+  void update(sf::Vector2f& movement[]);
   
   void renderBlooms(sf::RenderTarget& target);
   void render(sf::RenderTarget& target);
